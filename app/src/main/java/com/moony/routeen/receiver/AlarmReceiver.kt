87b64ed3164981f5546f2fe275389b9c37ba7bae
Moony-H <moonylight247@gmail.com>
2022-05-13 20:40:55 +0900

    package and library

---
+++ app/src/main/java/com/moony/routeen/receiver/AlarmReceiver.kt
@@ -0,0 +1,11 @@
+package com.moony.routeen.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver:BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+
+    }
+}
