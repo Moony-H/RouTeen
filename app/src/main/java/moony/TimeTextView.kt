9c8b15afa8d0c3c34cf05fc0606ec127f5e90554
Moony-H <moonylight247@gmail.com>
2022-05-17 21:50:53 +0900

    custom

---
+++ app/src/main/java/moony/TimeTextView.kt
@@ -0,0 +1,14 @@
+package moony
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class TimeTextView: ConstraintLayout {
+    constructor(context:Context):super(context){
+
+    }
+    constructor(context: Context,attrs:AttributeSet):super(context,attrs){
+
+    }
+}
